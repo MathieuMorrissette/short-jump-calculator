db1b3950774fa075406fff6f42a94f6aa0e0f0c1
Mathieu Morrissette <mmorrissette@devolutions.net>
2020-10-02 11:52:49 -0400

    add reverse jump calculator

--- reverse-jump.py
+++ reverse-jump.py
@@ -1,19 +1,18 @@
 
+
 current_address = int(input("Current adress :"), 16)
 dst_address = int(input("Destination address :"), 16)
 
-offset = dst_address - current_address
+offset = current_address - dst_address
 
 if offset <= 0:
     print("Error negative or zero offset")
     exit(0)
 
+code = int("0xFF", 16) - ((offset + 1))
 
-code = offset - 2
-
-if(code > 127):
+if code > 128:
     print("Error cannot jump further than 127 bytes")
-    exit(0)
+
 
 print("JUMP CODE : EB " + (hex(code)[2:].zfill(2)).upper())
-
